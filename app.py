d62888536d69487fdd448211ff376aad9f2547cd
H. M. Mahedi Hasan <jisan2723@gmail.com>
2018-01-04 11:55:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python
-
 from __future__ import print_function
 from future.standard_library import install_aliases
-install_aliases()
 
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
+install_aliases()
 
 import json
 import os
@@ -15,11 +10,8 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
-from googleapiclient.discovery import build
-import pprint
-
-# Flask app should start in global layout
 app = Flask(__name__)
+
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -28,67 +20,45 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = ProcessRequest(req)
 
     res = json.dumps(res, indent=4)
-    # print(res)
+    print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
 
 # construct search query from result.parameters
-def processRequest(req):
-
-    if req.get("result").get("action") != "googleSearch":
+def ProcessRequest(req):
+    if req.get("result").get("action") != "Maths":
         return {}
     json_params = req.get("result").get("parameters")
-    searchstring = ''    # this creates the overall topic which covers user's raw query
+    take = json_params.split()
 
-    for value in json_params.values():
-        searchstring += value
-        searchstring += " "
-    print(searchstring)
-    searchString = "robot %s" % searchstring
+    sum = 0
 
-    # KEYS SHOULDNT BE DISPLAYED
-    my_api_key = ""
-    my_cse_id = ""
-    searchResults = google_search(searchString, my_api_key, my_cse_id, num=1)    # search for the topic
+    if take[1] == '+':
+        sum = int(take[0] + '') + int(take[2] + '')
+        # print(sum)
 
-    if searchResults is None:
-        return{}
+    if take[1] == '-':
+        sum = int(take[0] + '') - int(take[2] + '')
+        # print(sum)
 
-    res = makeWebhookResult(searchResults, searchstring)
+    if take[1] == '*':
+        sum = int(take[0] + '') * int(take[2] + '')
+        # print(sum)
+
+    if take[1] == '/':
+        if int(take[2]) != 0:
+            sum = int(take[0] + '') / int(take[2] + '')
+            # print(sum)
+        else:
+            return "Invalid"
+
+    res = sum
     return res
-
-
-def google_search(search_term, api_key, cse_id, **kwargs):
-    service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-    return res['items']
-
-
-def makeWebhookResult(data, searchstring):
-    if (data[0] is None):
-        return {}
-
-    articleUrl = data[0].get('formattedUrl')
-    # print(json.dumps(item, indent=4))
-
-    speech = "Please view this article for more information on " + searchstring + ": " \
-             + articleUrl
-
-    print("Response:")
-    print(speech)
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "google-search-webhook"
-    }
 
 
 if __name__ == '__main__':
@@ -97,3 +67,4 @@ if __name__ == '__main__':
     print("Starting app on port %d" % port)
 
     app.run(debug=False, port=port, host='0.0.0.0')
+
