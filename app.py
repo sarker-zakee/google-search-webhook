ec62e52abe20ce09d0cc4574479264a122dfc2db
H. M. Mahedi Hasan <jisan2723@gmail.com>
2018-01-08 12:18:09 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-from __future__ import print_function
-from future.standard_library import install_aliases
-
-install_aliases()
-
 import json
 import os
 
@@ -13,15 +8,12 @@ from flask import make_response
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
-    print("Request:")
+    print("Request: ")
     print(json.dumps(req, indent=4))
-
-    res = ProcessRequest(req)
-
+    res = MakeWebRequest(req)
     res = json.dumps(res, indent=4)
     print(res)
     r = make_response(res)
@@ -29,28 +21,26 @@ def webhook():
     return r
 
 
-# construct search query from result.parameters
-def ProcessRequest(req):
-    if req.get("result").get("action") != "Maths":
+def MakeWebRequest(req):
+    if req.get("request").get("action") != "interest":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    value1 = parameters.get("number")
-    value2 = parameters.get("number1")
-
-    sum = int(value1) + int(value2)
-    res = "The sum of two number is: " + str(sum)
+    names = parameters.get("org-name")
+    organization = {'org1': '10', 'org2': '20', 'org3': '30'}
+    speech = "The organization's of " + names + " small projects are: " + str(organization[names])
+    print("Response: ")
+    print(speech)
     return {
-        "speech": res,
-        "displayText": res,
+        "speech": speech,
+        "displayText": speech,
         "source": "Athena"
     }
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 80))
+    port = int(os.getenv('POST', 5000))
+    print("Starting app on %d" % port)
+    app.run(debug=True, port=port, host='0.0.0.0')
 
-    print("Starting app on port %d" % port)
-
-    app.run(debug=False, port=port, host='0.0.0.0')
 
